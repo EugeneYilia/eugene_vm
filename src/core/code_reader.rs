7685c566276958a3bc66dd95a316df41f22fdc5f
EugeneLiu <583076221@qq.com>
2021-09-22 01:08:56 +0800

    +: add part of code_reader

--- src/core/code_reader.rs
+++ src/core/code_reader.rs
@@ -3,3 +3,23 @@ pub struct CodeReader {
     pub pc: usize,
 }
 
+impl CodeReader {
+    pub fn new(code: Vec<u8>) -> CodeReader {
+        CodeReader { code, pc: 0 }
+    }
+
+    pub fn read_u8(mut self) -> u8 {
+        let u8_value = self.code[self.pc];
+        self.pc += 1;
+        u8_value
+    }
+
+    pub fn read_i8(mut self)->i8{
+        let u8_value = self.code[self.pc];
+        let i8_value = unsafe {std::mem::transmute::<u8,i8>(u8_value)};
+        self.pc += 1;
+        i8_value
+    }
+
+    pub fn read_
+}
