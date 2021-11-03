88dcf06811f6eb6f23c13f274d4868acba031767
EugeneLiu <583076221@qq.com>
2021-11-04 01:21:26 +0800

    +:  添加class_reader相关内容

--- src/core/class_loader/class_reader.rs
+++ src/core/class_loader/class_reader.rs
@@ -3,6 +3,11 @@ use crate::runtime::method_area::constant_pool::constant_info::ConstantInfo;
 use crate::runtime::method_area::constant_pool::ConstantPool;
 use crate::runtime::method_area::constant_pool::constant_pool::ConstantPool;
 use crate::runtime::method_area::classfile::member_info::MemberInfo;
+use crate::runtime::method_area::classfile::attribute_info::exception_table_entry::ExceptionTableEntry;
+use crate::runtime::method_area::classfile::attribute_info::line_number_table_entry::LineNumberTableEntry;
+use crate::runtime::method_area::classfile::attribute_info::local_variable_table_entry::LocalVariableTableEntry;
+use crate::runtime::method_area::classfile::attribute_info::attribute_info::AttributeInfo;
+use crate::runtime::method_area::classfile::classfile::ClassFile;
 
 trait ClassReader {
     fn read_u8(&self) -> (u8, &[u8]);
@@ -19,15 +24,121 @@ trait ClassReader {
     fn read_bytes(&self, n: usize) -> (&[u8], &[u8]);
 
     fn read_and_check_magic_number(&self) -> (u32, &[u8]);
-    fn read_and_check_version(&self)->(VersionInfo,&[u8]);
-    fn read_constant_info(&self)->(ConstantInfo,&[u8]);
-    fn read_constant_pool(&self)->(ConstantPool,&[u8]);
-    fn read_access_flags(&self)->(u16,&[u8]);
-    fn read_this_class(&self)->(u16,&[u8]);
-    fn read_super_class(&self)->(u16,&[u8]);
-    fn read_interfaces(&self)->(Vec<u16>,&[u8]);
-    fn read_member(&self, constant_pool:&ConstantPool) -> (MemberInfo,&[u8]);
-    fn read_members(&self, constant_pool:&ConstantPool) -> (Vec<MemberInfo>,&[u8]);
+    fn read_and_check_version(&self) -> (VersionInfo, &[u8]);
+    fn read_constant_info(&self) -> (ConstantInfo, &[u8]);
+    fn read_constant_pool(&self) -> (ConstantPool, &[u8]);
+    fn read_access_flags(&self) -> (u16, &[u8]);
+    fn read_this_class(&self) -> (u16, &[u8]);
+    fn read_super_class(&self) -> (u16, &[u8]);
+    fn read_interfaces(&self) -> (Vec<u16>, &[u8]);
+    fn read_member(&self, constant_pool: &ConstantPool) -> (MemberInfo, &[u8]);
+    fn read_members(&self, constant_pool: &ConstantPool) -> (Vec<MemberInfo>, &[u8]);
+    fn read_exception_table(&self) -> (Vec<ExceptionTableEntry>, &[u8]);
+    fn read_line_number_table(&self) -> (Vec<LineNumberTableEntry>, &[u8]);
+    fn read_local_variable_table(&self) -> (Vec<LocalVariableTableEntry>, &[u8]);
+    fn read_attribute(&self, constant_pool: &ConstantPool) -> (AttributeInfo, &[u8]);
+    fn find_attributes(&self, constant_pool: &ConstantPool) -> (Vec<AttributeInfo>, &[u8]);
+    fn parse(&self) -> ClassFile;
+}
 
+impl ClassReader for [u8]  {
+    fn read_u8(&self) -> (u8, &[u8]) {
+        todo!()
+    }
 
+    fn read_u16(&self) -> (u16, &[u8]) {
+        todo!()
+    }
+
+    fn read_u16s(&self) -> (Vec<u16>, &[u8]) {
+        todo!()
+    }
+
+    fn read_u32(&self) -> (u32, &[u8]) {
+        todo!()
+    }
+
+    fn read_i32(&self) -> (i32, &[u8]) {
+        todo!()
+    }
+
+    fn read_i64(&self) -> (i64, &[u8]) {
+        todo!()
+    }
+
+    fn read_f32(&self) -> (f32, &[u8]) {
+        todo!()
+    }
+
+    fn read_f64(&self) -> (f64, &[u8]) {
+        todo!()
+    }
+
+    fn read_bytes(&self, n: usize) -> (&[u8], &[u8]) {
+        todo!()
+    }
+
+    fn read_and_check_magic_number(&self) -> (u32, &[u8]) {
+        todo!()
+    }
+
+    fn read_and_check_version(&self) -> (VersionInfo, &[u8]) {
+        todo!()
+    }
+
+    fn read_constant_info(&self) -> (ConstantInfo, &[u8]) {
+        todo!()
+    }
+
+    fn read_constant_pool(&self) -> (ConstantPool, &[u8]) {
+        todo!()
+    }
+
+    fn read_access_flags(&self) -> (u16, &[u8]) {
+        todo!()
+    }
+
+    fn read_this_class(&self) -> (u16, &[u8]) {
+        todo!()
+    }
+
+    fn read_super_class(&self) -> (u16, &[u8]) {
+        todo!()
+    }
+
+    fn read_interfaces(&self) -> (Vec<u16>, &[u8]) {
+        todo!()
+    }
+
+    fn read_member(&self, constant_pool: &ConstantPool) -> (MemberInfo, &[u8]) {
+        todo!()
+    }
+
+    fn read_members(&self, constant_pool: &ConstantPool) -> (Vec<MemberInfo>, &[u8]) {
+        todo!()
+    }
+
+    fn read_exception_table(&self) -> (Vec<ExceptionTableEntry>, &[u8]) {
+        todo!()
+    }
+
+    fn read_line_number_table(&self) -> (Vec<LineNumberTableEntry>, &[u8]) {
+        todo!()
+    }
+
+    fn read_local_variable_table(&self) -> (Vec<LocalVariableTableEntry>, &[u8]) {
+        todo!()
+    }
+
+    fn read_attribute(&self, constant_pool: &ConstantPool) -> (AttributeInfo, &[u8]) {
+        todo!()
+    }
+
+    fn find_attributes(&self, constant_pool: &ConstantPool) -> (Vec<AttributeInfo>, &[u8]) {
+        todo!()
+    }
+
+    fn parse(&self) -> ClassFile {
+        todo!()
+    }
 }
--- src/runtime/method_area/classfile/attribute_info/mod.rs
+++ src/runtime/method_area/classfile/attribute_info/mod.rs
@@ -1,5 +1,5 @@
 pub mod attribute_info;
 
-mod exception_table_entry;
-mod line_number_table_entry;
-mod local_variable_table_entry;
+pub mod exception_table_entry;
+pub mod line_number_table_entry;
+pub mod local_variable_table_entry;
