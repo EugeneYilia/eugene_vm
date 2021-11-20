d8c827b4c21bd789e03789100f2cf38d3335c4c2
EugeneLiu <583076221@qq.com>
2021-11-20 13:23:59 +0800

    +: 完成class_loader部分

--- src/core/class_loader/class_loader.rs
+++ src/core/class_loader/class_loader.rs
@@ -10,6 +10,10 @@ use crate::runtime::method_area::class::method::Method;
 use crate::runtime::method_area::constant_pool::constant_pool::ConstantPool;
 use crate::runtime::stack::variables_table::VariableTable;
 use crate::constants::access_flags::{ACCESS_STATIC, ACCESS_FINAL};
+use crate::constants::field_descriptor::*;
+use crate::runtime::method_area::constant_pool::constant_info::ConstantInfo;
+use crate::runtime::stack::variable_slot::VariableSlot;
+use crate::util::converter;
 
 ///       下一个实例字段slot_id  下一个静态字段slot_id  static变量表  常量池
 type SlotIdAccumulator = (usize, usize, VariableTable, ConstantPool);
@@ -76,16 +80,58 @@ impl ClassLoader {
 
 
         fn calc_instance_slot_id(slot_id_accumulator: SlotIdAccumulator, field: &Field) -> SlotIdAccumulator {
-            let (next_instance_slot_id, next_static_slot_id, static_variable_table, constant_pool) = slot_id_accumulator;
+            let (next_instance_slot_id, next_static_slot_id, mut static_variable_table, constant_pool) = slot_id_accumulator;
             let used_slot_amount = if field.is_need_two_slot() { 2usize } else { 1usize };
             if field.get_access_flags() & ACCESS_STATIC != 0 {
-                if field.get_access_flags() & ACCESS_FINAL != 0 {
-                    // static final 会被编译为常量 内联到使用的地方
-                
+                if field.constant_value_index.is_none() {
+                    // 此时应该是static final variable_name = primitive value  会被编译为常量 内联到使用的地方 不会更新static variable table 和 next_static_slot_id
+                    (next_instance_slot_id, next_static_slot_id, static_variable_table, constant_pool)
                 } else {
-
+                    // 添加到static variable table中
+                    let constant_value_index = field.constant_value_index.unwrap();
+                    match field.get_descriptor() {
+                        BYTE_FIELD_DESCRIPTOR | CHAR_FIELD_DESCRIPTOR | INT_FIELD_DESCRIPTOR | SHORT_FIELD_DESCRIPTOR | BOOLEAN_FIELD_DESCRIPTOR | OBJ_FIELD_DESCRIPTOR => {
+                            match constant_pool.get(constant_value_index) {
+                                ConstantInfo::Integer(value) => {
+                                    static_variable_table.set_variable_slot(next_static_slot_id, VariableSlot::I32(*value))
+                                }
+                                _ => panic!("constant_value_index: {} is not ConstantInfo::Integer")
+                            }
+                        }
+                        DOUBLE_FIELD_DESCRIPTOR => {
+                            match constant_pool.get(constant_value_index) {
+                                ConstantInfo::Double(value) => {
+                                    let [first, second] = converter::f64_to_i32seq(*value);
+                                    static_variable_table.set_variable_slot(next_static_slot_id, VariableSlot::I32(first));
+                                    static_variable_table.set_variable_slot(next_static_slot_id + 1, VariableSlot::I32(second));
+                                }
+                                _ => panic!("constant_value_index: {} is not ConstantInfo::Double")
+                            }
+                        }
+                        FLOAT_FIELD_DESCRIPTOR => {
+                            match constant_pool.get(constant_value_index) {
+                                ConstantInfo::Float(value) => {
+                                    static_variable_table.set_variable_slot(next_static_slot_id, VariableSlot::I32(converter::f32_to_i32(*value)))
+                                }
+                                _ => panic!("constant_value_index: {} is not ConstantInfo::Float")
+                            }
+                        }
+                        LONG_FIELD_DESCRIPTOR => {
+                            match constant_pool.get(constant_value_index) {
+                                ConstantInfo::Long(value) => {
+                                    let [first, second] = converter::i64_to_i32seq(*value);
+                                    static_variable_table.set_variable_slot(next_static_slot_id, VariableSlot::I32(first));
+                                    static_variable_table.set_variable_slot(next_static_slot_id + 1, VariableSlot::I32(second));
+                                }
+                                _ => panic!("constant_value_index: {} is not ConstantInfo::Long")
+                            }
+                        }
+                        _ => {
+                            panic!("Invalid descriptor type: {}", field.get_descriptor())
+                        }
+                    }
+                    (next_instance_slot_id, next_static_slot_id + used_slot_amount, static_variable_table, constant_pool)
                 }
-                (next_instance_slot_id + used_slot_amount, next_static_slot_id, static_variable_table, constant_pool)
             } else {
                 (next_instance_slot_id + used_slot_amount, next_static_slot_id, static_variable_table, constant_pool)
             }
@@ -97,18 +143,21 @@ impl ClassLoader {
 
         let slot_id_accumulator: SlotIdAccumulator = (next_instance_slot_id, 0usize, VariableTable::new(), constant_pool);
 
-        todo!()
-        // let class_ref = Rc::new(Class {
-        //     access_flags,
-        //     constant_pool,
-        //     class_name,
-        //
-        //     methods,
-        //     super_class,
-        //
-        // });
-        //
-        // (class_loader, class_ref)
+        let (next_instance_slot_id, next_static_slot_id, static_variable_table, constant_pool) = fields.iter().fold(slot_id_accumulator, calc_instance_slot_id);
+
+        let class_ref = Rc::new(Class {
+            access_flags,
+            constant_pool,
+            class_name,
+            fields,
+            methods,
+            super_class,
+            next_instance_slot_id,
+            next_static_slot_id,
+            static_variable_table
+        });
+
+        (class_loader, class_ref)
     }
 }
 
