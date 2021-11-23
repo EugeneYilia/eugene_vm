346f7fd35c0e36e6742feb92041518cbf1064f2d
EugeneLiu <liuyichen@ushareit.com>
2021-11-23 17:41:38 +0800

    feat: 完成新增字节码指令的注册

--- src/constants/instruction_constants.rs
+++ src/constants/instruction_constants.rs
@@ -2,6 +2,7 @@ use crate::core::bytecode_execution_engine::instruction::control::goto::goto;
 use crate::core::bytecode_execution_engine::instruction::instruction_execute_result::InstructionExecuteResult;
 use crate::core::bytecode_execution_engine::instruction::load::iload::{iload_0, iload_1, iload_2, iload_3};
 use crate::core::bytecode_execution_engine::instruction::math::add::{dadd, fadd, iadd, ladd};
+use crate::core::bytecode_execution_engine::instruction::math::and::{iand,land};
 use crate::core::bytecode_execution_engine::instruction::nop::nop;
 use crate::core::bytecode_execution_engine::instruction::store::istore::{istore_0, istore_1, istore_2, istore_3};
 use crate::core::code_reader::code_reader::CodeReader;
@@ -22,6 +23,8 @@ pub fn get_instruction_fn(instruction_op_code: u8) -> fn(&mut CodeReader, &mut T
         0x61 => ladd,
         0x62 => fadd,
         0x63 => dadd,
+        0x7e => iand,
+        0x7f => land,
         0xa7 => goto,
         _ => panic!("illegal instruction op code: {}", instruction_op_code)
     }
