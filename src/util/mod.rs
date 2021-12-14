97e62ca592ed98e5ab407fa7fcbc3457751ff645
EugeneLiu <liuyichen.destiny@bytedance.com>
2021-12-14 13:37:20 +0800

    +: 增加对于wrapping的测试

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,3 +1,25 @@
 pub mod class_util;
 pub mod converter;
 pub mod file_util;
+
+
+#[cfg(test)]
+mod tests {
+    use std::num::Wrapping;
+
+    #[test]
+    fn test_wrapping() {
+        let zero = Wrapping(0u8);
+        let one = Wrapping(1u8);
+        let result = zero -one;
+        println!("{}", result);
+    }
+
+    #[test]
+    fn test_add() {
+        let _zero = 0u8;
+        let _one = 1u8;
+        // let result = _zero - _one;
+        // println!("{}", result);
+    }
+}
