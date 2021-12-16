15fb9e390da803c4dcc7ee381bfc2a886bdab465
EugeneLiu <liuyichen.destiny@bytedance.com>
2021-12-17 01:29:25 +0800

    +: 补全String class

--- eugene_test/src_code/eugene_rt/lib/rt/java/lang/String.java
+++ eugene_test/src_code/eugene_rt/lib/rt/java/lang/String.java
@@ -1,5 +1,8 @@
 package java.lang;
 
+import java.nio.charset.StandardCharsets;
+
+
 /**
  * @author EugeneLiu
  * @date 2021-12-16
@@ -13,10 +16,28 @@ public final class String {
         System.arraycopy(value, 0, this.value, 0, value.length);
     }
 
-    public byte[] getBytes(){
+    /**
+     * JDK source code      char => byte[]
+     * private void put(char c, ByteBuffer dst) {
+     * if (byteOrder == BIG) {
+     * dst.put((byte)(c >> 8));
+     * dst.put((byte)(c & 0xff));
+     * } else {
+     * dst.put((byte)(c & 0xff));
+     * dst.put((byte)(c >> 8));
+     * }
+     * }
+     */
+    public byte[] getBytes() {
         // only support UTF-16 now
-        byte[] bytes = new byte[2 * this.value.length];
-
+        byte[] bytes = new byte[2 * this.value.length + 2];
+        bytes[0] = -2;
+        bytes[1] = -1;
+        for (int i = 0; i < value.length; i++) {
+            bytes[i * 2 + 2] = (byte) (value[i] >> 8);
+            bytes[i * 2 + 1 + 2] = (byte) (value[i] & 0xff);
+        }
         return bytes;
     }
 }
+
---
+++ eugene_test/src_code/test/TestString.java
@@ -0,0 +1,29 @@
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class TestString {
+    public static void main(String[] args) {
+        // [-2, -1, 0, 69, 0, 117, 0, 103, 0, 101, 0, 110, 0, 101, 0, 76, 0, 105, 0, 117]
+        String author = "EugeneLiu";
+        System.out.println(Arrays.toString(author.getBytes(StandardCharsets.UTF_16)));
+
+        System.out.println(Arrays.toString(getBytes(author.toCharArray())));
+
+        byte[] strangeArray = new byte[]{-2,-1};
+//        strangeArray.
+        String newString = new String(strangeArray, StandardCharsets.UTF_16);
+        System.out.println(newString);
+    }
+
+    public static byte[] getBytes(char[] value) {
+        // only support UTF-16 Big-Endian now
+        byte[] bytes = new byte[2 * value.length + 2];
+        bytes[0] = -2;
+        bytes[1] = -1;
+        for (int i = 0; i < value.length; i++) {
+            bytes[i * 2 + 2] = (byte) (value[i] >> 8);
+            bytes[i * 2 + 1 + 2] = (byte) (value[i] & 0xff);
+        }
+        return bytes;
+    }
+}
