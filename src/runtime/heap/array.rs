caadf50341effa020604f8e327dc707d9cd2c875
EugeneLiu <liuyichen@ushareit.com>
2021-12-18 19:01:34 +0800

    feat: 1. 初版对象和数组定义

--- src/runtime/heap/array.rs
+++ src/runtime/heap/array.rs
@@ -3,7 +3,7 @@ use crate::runtime::method_area::class::class::Class;
 use crate::runtime::stack::variable_slot::VariableSlot;
 
 #[derive(Debug)]
-pub struct Object {
+pub struct Array {
     class: Rc<Class>,
-    fields: Vec<VariableSlot>,
+    values: Vec<VariableSlot>,
 }
